200b212db8fc6b3997765a27d6d8a1ebcf636091
Altia <soporte.owcsgp@altia.es>
2023-03-23 13:30:03 +0100

    Front end development.

--- src/main/java/diamondEngine/diaRenderer/Texture.java
+++ src/main/java/diamondEngine/diaRenderer/Texture.java
@@ -1,5 +1,7 @@
 package diamondEngine.diaRenderer;
 
+import diamondEngine.diaUtils.DiaLogger;
+import diamondEngine.diaUtils.DiaLoggerLevel;
 import org.lwjgl.BufferUtils;
 
 import java.nio.ByteBuffer;
@@ -14,6 +16,7 @@ public class Texture {
     private int id;
     private int width;
     private int height;
+    private String path;
 
     // CONTRUCTORS
     public Texture() {
@@ -53,7 +56,65 @@ public class Texture {
         this.height = height;
     }
 
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
     // METHODS
-    public void init () {
+    public void load(String path) {
+
+        this.path = path;
+        this.id = glGenTextures();
+        glBindTexture(GL_TEXTURE_2D, id);
+
+        // Texture Parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+        IntBuffer x = BufferUtils.createIntBuffer(1);
+        IntBuffer y = BufferUtils.createIntBuffer(1);
+        IntBuffer channels = BufferUtils.createIntBuffer(1);
+        stbi_set_flip_vertically_on_load(true);
+        ByteBuffer image = stbi_load(path, x, y, channels, 0);
+
+        if (image != null) {
+            this.width = x.get(0);
+            this.height = y.get(0);
+            if (channels.get(0) == 4) {
+                // RGBA Image
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, x.get(0), y.get(0), 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
+            } else if (channels.get(0) == 3) {
+                // RGB Image
+                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, x.get(0), y.get(0), 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+            } else {
+                DiaLogger.log("Failed to load texture, unexpected number of channels", DiaLoggerLevel.ERROR);
+            }
+        } else {
+            DiaLogger.log("Failed to load texture from '" + path + "'", DiaLoggerLevel.ERROR);
+        }
+        stbi_image_free(image);
+    }
+
+    public void bind() {
+        glBindTexture(GL_TEXTURE_2D, id);
+    }
+
+    public void unbind() {
+        glBindTexture(GL_TEXTURE_2D, 0);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (!(o instanceof Texture)) return false;
+        Texture tex = (Texture) o;
+        return tex.getWidth() == this.width && tex.getHeight() == this.height && tex.getId() == this.id &&
+                tex.getPath().equals(this.path);
     }
 }
